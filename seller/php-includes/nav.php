e0c790850c1dd993617f69245221e24bab323b1c
aimedidierm <aimedidiermugisha@gmail.com>
2022-10-11 10:11:43 +0200

    Seller part

--- seller/php-includes/nav.php
+++ seller/php-includes/nav.php
@@ -60,13 +60,13 @@ $email="seller@gmail.com";
   <ul class="navbar-nav align-items-center right-nav-link">
     <li class="nav-item">
       <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" data-toggle="dropdown" href="#">
-        <span class="user-profile"><img src="https://via.placeholder.com/110x110" class="img-circle" alt="user avatar"></span>
+        <span class="user-profile"><img src="../assets/images/user.jpg" class="img-circle" alt="user avatar"></span>
       </a>
       <ul class="dropdown-menu dropdown-menu-right">
        <li class="dropdown-item user-details">
         <a href="javaScript:void();">
            <div class="media">
-             <div class="avatar"><img class="align-self-start mr-3" src="https://via.placeholder.com/110x110" alt="user avatar"></div>
+             <div class="avatar"><img class="align-self-start mr-3" src="../assets/images/user.jpg" alt="user avatar"></div>
             <div class="media-body">
             <h6 class="mt-2 user-title"><?php echo $names;?></h6>
             <p class="user-subtitle"><?php echo $email;?></p>
