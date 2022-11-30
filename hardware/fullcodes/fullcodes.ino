0e9604e6afa4996f9b8372659508420dc57f0bba
aimedidierm <aimedidiermugisha@gmail.com>
2022-11-30 21:57:33 +0200

    hardware

--- hardware/fullcodes/fullcodes.ino
+++ hardware/fullcodes/fullcodes.ino
@@ -1,19 +1,24 @@
 #include <ArduinoJson.h>
-#include <LiquidCrystal_I2C.h>
 #include <SPI.h>
 #include <MFRC522.h>
-int lcdColumns = 16;
-int lcdRows = 2;
+#include <LiquidCrystal.h>
 
-LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
 
-#define SS_PIN 10
-#define RST_PIN 9
-#define buzzer 3
-#define led 2
-int motor1=6;
-int motor2=7;
-const int buto = 8;  
+const int rs = A6, en = A7, d4 = A11, d5 = A10, d6 = A9, d7 = A8;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+#define SS_PIN 53
+#define RST_PIN 5
+#define buzzer A0
+#define led A1
+int motor1=4;
+int motor2=6;
+int motor1u=2;
+int motor2u=3;
+const int buto = A4;
+const int butou = A5;
+const int rbuto = A2;
+const int rbuto2 = A3;
 byte readCard[4];
 int k=0;
 String tagID = "";
@@ -21,46 +26,54 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 boolean getID();
 int buttonState = 0;
+int rbuttonState = 0;
+int rbuttonState2 = 0;
+int buttonStateu = 0;
 void setup() {
+lcd.begin(16, 2);
+
+ Serial.begin(115200);
 pinMode(buzzer, OUTPUT); 
 pinMode(led, OUTPUT);
 pinMode(motor1,OUTPUT);
 pinMode(motor2,OUTPUT);
+pinMode(motor1u,OUTPUT);
+pinMode(motor2u,OUTPUT);
 pinMode(buto, INPUT);
+pinMode(butou, INPUT);
+pinMode(rbuto, INPUT);
+pinMode(rbuto2, INPUT);
+
 
   SPI.begin();
   mfrc522.PCD_Init();
-  lcd.init();
-  lcd.backlight();
   lcd.setCursor(2, 0);
   lcd.print("Smart public");
   lcd.setCursor(5, 1);
   lcd.print("toilette");
   delay(3000);
-  Serial.begin(9600);
 }
 
 void loop() {
- Serial.begin(115200);
  lcd.clear();
  lcd.setCursor(0, 0);
  lcd.print("Place Your Card");    
 
   if (getID()){
-    if (tagID == "B177EA1B"){
+    if (tagID == "41FCFE45"){
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("Welcome admin");
+    lcd.print("Welcome");
     tone(buzzer, 1000, 1000);
     delay(2000);
     opendoor();
-      } else if (tagID == "A38FACAB"){
+      } else if (tagID == "B38204B"){
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("Welcome cleaner");
+    lcd.print("Welcome");
     tone(buzzer, 1000, 1000);
     delay(2000);
-    opendoor();
+    opendooru();
       } else{
         Serial.println((String)"?card="+tagID);
         while(k==0){
@@ -124,16 +137,55 @@ void opendoor(){
   lcd.print("Somebody in");
   opendoor2();
   }
+  void opendooru(){
+  digitalWrite(motor1u,HIGH);
+  digitalWrite(motor2u,LOW);
+  delay(5000);
+  digitalWrite(motor2u,HIGH);
+  digitalWrite(motor1u,LOW);
+  digitalWrite(led,HIGH);
+  delay(2000);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Somebody in");
+  opendoor2u();
+  }
 void opendoor2(){
   int i = 1;
   while(i>0){
  buttonState = digitalRead(buto);
+ rbuttonState = digitalRead(rbuto);
+ if (rbuttonState == HIGH) {
+  Serial.println("?report");
+  delay(1000);
+ }
+ }
   if (buttonState == HIGH) {
   digitalWrite(motor1,HIGH);
   digitalWrite(motor2,LOW);
   delay(5000);
   digitalWrite(motor2,HIGH);
   digitalWrite(motor1,LOW);
+  digitalWrite(led,LOW);
+  resetFunc();
+  }
+  delay(200);
+  }
+void opendoor2u(){
+  int i = 1;
+  while(i>0){
+ buttonStateu = digitalRead(butou);
+ rbuttonState2 = digitalRead(rbuto2);
+ if (rbuttonState2 == HIGH) {
+  Serial.println("?report2");
+  delay(1000);
+ }
+  if (buttonStateu == HIGH) {
+  digitalWrite(motor1u,HIGH);
+  digitalWrite(motor2u,LOW);
+  delay(5000);
+  digitalWrite(motor2u,HIGH);
+  digitalWrite(motor1u,LOW);
   digitalWrite(led,LOW);
   resetFunc();
   }
